ae4c569eac47728779aaa70691d18ca9c43f4aee
angelgracci <angelgracci@outlook.com>
2024-10-31 20:04:07 -0300

    modificar comportamiento boton menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const toggleMenu = () => {
+    const tdc = document.getElementById('tdc')
+    tdc.innerText = "TRABAJA EN TCD" ;
     let classname = 'menuDesplegableActive'
     menuDesplegable.classList.toggle(classname)
 };
@@ -6,4 +8,10 @@ const menuDesplegable = document.getElementById('menuDesplegableInactive')
 const toggleButton = document.getElementById('toggle'); 
 toggleButton.addEventListener('click', () => {
     toggleMenu();
+});
+
+window.addEventListener('click', (event) => {
+    if (menuDesplegable.classList.contains('menuDesplegableActive') && !menuDesplegable.contains(event.target) && !toggleButton.contains(event.target)) {
+        menuDesplegable.classList.remove('menuDesplegableActive');
+    }
 });
